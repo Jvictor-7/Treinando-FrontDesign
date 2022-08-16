d1c391d383e57936aa4b0530956188e1fe01cb26
jhonata <jhonatatnt@hotmail.com>
2022-08-16 12:36:23 -0300

    Update home.php

--- Views/home.php
+++ Views/home.php
@@ -4,10 +4,43 @@
 
 	include('../includes/mn_pri.php');
 ?>
+	
+	<div class="div"><a href="">Plataforma/Home</a></div>
+
 
 	<!-- CENTER -->
-	<div class = "containerCss">
-    </div>
+	<div class="container">
+		<style type="text/css">
+			.container{
+				display: flex;
+				flex-direction: row;
+				width: 100%;
+			}
+			.notifications, .news{
+				display: flex;
+				width: calc(50% - 15px);
+				height: 100px;
+				background-color: #000;
+			}
+			.news{
+				margin-left: 10px;
+				margin-right: 5px;
+			} 
+			.notifications{
+				margin-left: 5px;
+				margin-right: 10px;
+			}
+		</style>
+
+		<div class="news">
+
+		</div>
+
+		<div class="notifications">
+
+		</div>
+	</div>
+	
     <!-- //CENTER -->
 
 
