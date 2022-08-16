b7784edd7e7288ff2f90ce313287f4a6b5556c9a
jhonata <jhonatatnt@hotmail.com>
2022-08-16 12:17:01 -0300

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -7,8 +7,8 @@
     <meta name="description" content="Desenvolvendo o novo design das páginas do projeto de Monitoria...">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Design</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/mnbar.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/mnbar.css">
 </head>
 
 <body>
