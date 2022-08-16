5c0a05d0726bd7e14f8112daae2eafd93930c15e
jhonata <jhonatatnt@hotmail.com>
2022-08-16 12:17:13 -0300

    Create comunicacao.php

---
+++ Views/comunicacao.php
@@ -0,0 +1,15 @@
+<?php
+	include('../includes/head.php');
+	include('../includes/top_bar.php');
+
+	include('../includes/mn_pri.php');
+?>
+
+	<!-- CENTER -->
+	<div class = "containerCss">
+    </div>
+    <!-- //CENTER -->
+
+
+</body>
+</html>
