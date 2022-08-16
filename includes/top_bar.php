0f741fca2b2fdf24e4060d1c63c7eec3b53c0047
jhonata <jhonatatnt@hotmail.com>
2022-08-16 11:53:58 -0300

    Create top_bar.php

---
+++ includes/top_bar.php
@@ -0,0 +1,8 @@
+ <!-- TOP BAR -->
+ <nav class = "topBarCss">
+        <div class = "titleCss">
+            <img src="img/brasao-ufal.png" alt="" width="25px">
+            <div> UFAL | Monitoria </div>
+        </div>
+    </nav>
+    <!-- //TOP BAR -->
