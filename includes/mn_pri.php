277d40c471fc8589003251e9bd5c05ec93f31566
jhonata <jhonatatnt@hotmail.com>
2022-08-16 12:17:07 -0300

    Update mn_pri.php

--- includes/mn_pri.php
+++ includes/mn_pri.php
@@ -2,21 +2,21 @@
 <div id="mn_pri">
     <div id="search_bar">
         <input type="text" value="O que você Procura?" name="pesquisa">
-        <button><img src="img/icn_search.png"></button>
+        <button><img src="../img/icn_search.png"></button>
     </div>
     <div id="it_options">
         <ul>
-            <li><a href="#" style="color:">Home</a></li>
-            <li><a href="#">Cadastro</a></li>
-            <li><a href="#">Comunicação</a></li>
-            <li><a href="#">Monitoria</a></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="cadastro.php">Cadastro</a></li>
+            <li><a href="comunicacao.php">Comunicação</a></li>
+            <li><a href="monitoria.php">Monitoria</a></li>
         </ul>
         <div id="bar_selection"></div>
     </div>
     <div id="it_perfil">
         <h2>Bem vindo, Pikachu</h2>
         <img src="https://yt3.ggpht.com/ytc/AKedOLTC0WZ_NhOU0cGxMrIlEug-0nbCbOQBgUA1-TCb=s900-c-k-c0x00ffffff-no-rj">
-        <button><img src="img/icn_msg.png"></button>
+        <button><img src="../img/icn_msg.png"></button>
     </div>
 </div>
 <!-- //MENU PRINCIPAL -->
