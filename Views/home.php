37b50f7c1cb5716e4bbff68dd01a61bcad1445cb
jhonata <jhonatatnt@hotmail.com>
2022-08-24 20:56:41 -0300

    Update home.php

--- Views/home.php
+++ Views/home.php
@@ -5,134 +5,243 @@
 	include('../includes/mn_pri.php');
 ?>
 	
-	<div class="name">
-		<style type="text/css">
-			.name{
-				display: block;
-				width: calc(100% - 80px);
-				margin: 30px  40px;
-			}
-			.name a{
-			color: #142748;
-			font-family: 'segoe ui'; 
-			font-size: 16px;
-			text-decoration: none;
-			}
-		</style>
-
-		<a href="">Plataforma</a><a href=""> / Home</a>
-
-	</div>
+	
 
 
 	<!-- CENTER -->
 	<div class="container">
+		<div class="name">
+			<style type="text/css">
+				.name{
+					display: block;
+					width: calc(100% - 80px);
+					margin: 30px  40px;
+				}
+				.name a{
+				color: #142748;
+				font-family: 'segoe ui'; 
+				font-size: 16px;
+				text-decoration: none;
+				}
+			</style>
+
+			<a href="../">Plataforma</a><a href=""> / Home</a>
+
+		</div>
+		
 		<style type="text/css">
 			.container{
-				display: flex;
-				flex-direction: row;
-				width: 100%;
+				display: block;
+				width: 1280px;
+				height: 100vh;
+				margin: 0 auto;
 			}
 			.notifications, .news{
 				display: block;
+				float: left;
 				height: calc(100vh - 225px);
-			}
+				}
+				.notifications h1, .news h1{
+					display: block;
+					width: 100%;
+					font-size: 22px;
+					font-family: 'Segoe UI';
+					font-weight: normal;
+				}
+				
+				.notifications{
+					width: calc(35% - 45px);
+					margin-left: 5px;
+					margin-right: 40px;
+				}
+				
+				.borda{
+					display: block;
+					margin: 5px 0 25px 0;
+					width: 45px;
+					height: 4px;
+					background-color: #000;
+					border-radius: 2px;
+				}
+
 			.news{
 				width: calc(65% - 45px);
 				margin-left: 40px;
 				margin-right: 5px;
-			} 
-			.notifications{
-				width: calc(35% - 45px);
-				margin-left: 5px;
-				margin-right: 40px;
-			}
-
-			.notifications h1, .news h1{
-				display: block;
-				width: 100%;
-				font-size: 22px;
-				font-family: 'Segoe UI';
-				font-weight: normal;
-			}
-			.borda{
-				display: block;
-				margin: 5px 0 25px 0;
-				width: 45px;
-				height: 4px;
-				background-color: #000;
-				border-radius: 2px;
-			}
-
-			.post{
-				display: block;
-				width: 100%;
-				min-width: 360px;
-				max-width: 840px;
-				margin-bottom: 10px;
-				height: 220px;
-				background-color: #fff;
-				box-shadow: 0px 0px 0.25em #777;
-				
-				border-radius: 10px;
-				}
-				.post img{
+				} 
+				.post_news{
 					display: block;
-					float: left;
-					margin-right: 15px;
-					width: 50%;
-					min-width: 180px;
-					max-width: 420px;
+					width: calc(100% - 10px);
+					min-width: 360px;
+					max-width: 840px;
+					margin-bottom: 10px;
 					height: 220px;
-					background-color: #999;
-				}
-				.post h2, .post h3{
-					display: block;
-					float: left;
-					width: 50%;
-					min-width: 180px;
-					max-width: 420px;
-					font-family: 'Segoe UI';
-					font-weight: normal;
-				}
-				.post h2{margin-top:15px; font-size: 18px;}
-				.post h3{font-size: 12px;}
-				
-				.post p{ 
-					display: block;
-					float: left;
-					width: calc(50% - 30px);
-					min-width: 180px;
-					max-width: 420px;
-					font-family: 'Segoe UI';
+					background-color: #fff;
+					box-shadow: 0px 0px 0.25em #777;
+					border-radius: 15px;
 					overflow: hidden;
-					text-align: justify;
+					list-style: none;
+					}
+					.thumb{
+						display: block;
+						float: left;
+						width: 50%;
+						height: 220px;
+						background-color: #999;
+					}
+
+					.sep_post_news{
+						display: inline-block;
+						width: calc(50%);
+						height: 220px;
+						padding: 20px;
+						}
+						.post_news h2, .post_news h3{
+							
+							font-family: 'Segoe UI';
+							font-weight: normal;
+						}
+						.post_news h2{
+							margin: 6.5px 20px 0px 20px;
+							font-size: 18px;
+						}
+						.post_news h3{
+							margin: 0px 20px 15px 20px;
+							font-size: 12px;
+						}
+						
+						.post_news p{ 
+							display: block;
+							width: calc(100% - 40px);
+							min-height: 95px;
+							max-height: 95px;
+							padding-bottom: 6px;
+							margin: 6.5px 20px 5px 20px;
+							text-align: justify;
+							font-family: segoe ui;
+							font-size: 12px;
+						}
+
+			
+			.post_notif{
+				display: block;
+				width: 100%;
+				height: 75px;
+				margin-top: 15px;
+				list-style: none;
 				}
+
+				.post_notif img{
+					display: block;
+					float: left;
+					width: 75px;
+					height: 75px;
+					border-radius: 50px;
+					margin-right: 20px;
+					background-color: rgba(0, 0, 0, 0.15);
+				}
+
+				.notif_title{
+					display: block;
+					float: left;
+					width: 100px;
+					height: 15px;
+					margin-top: 20px;
+					background-color: rgba(0, 0, 0, 0.15);
+				}
+
+				.notif_subtitle{
+					display: block;
+					float: left;
+					width: 300px;
+					height: 15px;
+					margin-top: 5px;
+					background-color: rgba(0, 0, 0, 0.15);
+				}
+		
 		</style>
 
 		<div class="news">
 			<h1>Notícias</h1>
 			<div class="borda"></div>
+			
+			<ul>
+				<li class="post_news">
+					<img class="thumb" src="https://th.bing.com/th/id/R.dcb3a2f37a734b353d8f5f310aadfc75?rik=acfvmdZCVxAMAQ&pid=ImgRaw&r=0">
+					<div class="sep_post_news">
+						<h2>Titulo</h2>
+						<h3>Subtitulo</h3>
 
-			<div class="post">
-				<img src="#">
-				<div class="sep_post">
-					<h2>Titulo</h2>
-					<h3>Subtitulo</h3>
+						<p>
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto  
+						</p>
+					</div>
+				</li>
 
-					<p>Descricao:TextoTextoTextoTextoTextoTextoTexto
-					TextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-					TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-					TextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-					TextoTextoTextoTextoTextoTextoTextoTextoTexto.
-					</p>
-				</div>
-			</div>
+				<li class="post_news">
+					<img class="thumb" src="https://th.bing.com/th/id/R.dcb3a2f37a734b353d8f5f310aadfc75?rik=acfvmdZCVxAMAQ&pid=ImgRaw&r=0">
+					<div class="sep_post_news">
+						<h2>Titulo</h2>
+						<h3>Subtitulo</h3>
+
+						<p>
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto  
+						</p>
+					</div>
+				</li>
+
+				<li class="post_news">
+					<img class="thumb" src="https://th.bing.com/th/id/R.dcb3a2f37a734b353d8f5f310aadfc75?rik=acfvmdZCVxAMAQ&pid=ImgRaw&r=0">
+					<div class="sep_post_news">
+						<h2>Titulo</h2>
+						<h3>Subtitulo</h3>
+
+						<p>
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto  
+						</p>
+					</div>
+				</li>
+
+				<li class="post_news">
+					<img class="thumb" src="https://th.bing.com/th/id/R.dcb3a2f37a734b353d8f5f310aadfc75?rik=acfvmdZCVxAMAQ&pid=ImgRaw&r=0">
+					<div class="sep_post_news">
+						<h2>Titulo</h2>
+						<h3>Subtitulo</h3>
+
+						<p>
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto 
+							Texto Texto Texto Texto Texto Texto  
+						</p>
+					</div>
+				</li>
+
+				
+			</ul>
 		</div>
 
 		<div class="notifications">
 			<h1>Notíficações</h1>
 			<div class="borda"></div>
+
+			<ul>
+				<li class="post_notif">
+					<img src="https://th.bing.com/th/id/OIP.YPHnKUxGZLajSd77ecZ_YgHaCp?pid=ImgDet&rs=1">
+					<div class="notif_title"></div>
+					<div class="notif_subtitle"></div>
+				</li>
+				<li class="post_notif">
+					<img src="https://th.bing.com/th/id/OIP.YPHnKUxGZLajSd77ecZ_YgHaCp?pid=ImgDet&rs=1">
+					<div class="notif_title"></div>
+					<div class="notif_subtitle"></div>
+				</li>
+			</ul>
 		</div>
 	</div>
 	
