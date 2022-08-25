7a6ea91d27d66a7ed4eeb3203cf7433da58a9abc
jhonata <jhonatatnt@hotmail.com>
2022-08-24 21:03:22 -0300

    Update cadastro.php

--- Views/cadastro.php
+++ Views/cadastro.php
@@ -6,7 +6,34 @@
 ?>
 
 	<!-- CENTER -->
-	<div class = "containerCss">
+	<div class="container">
+		<div class="name">
+			<style type="text/css">
+				.name{
+					display: block;
+					width: calc(100% - 80px);
+					margin: 30px  40px;
+				}
+				.name a{
+				color: #142748;
+				font-family: 'segoe ui'; 
+				font-size: 16px;
+				text-decoration: none;
+				}
+			</style>
+
+			<a href="../">Plataforma</a><a href=""> / Cadastro</a>
+
+		</div>
+		
+		<style type="text/css">
+			.container{
+				display: block;
+				width: 1280px;
+				height: 100vh;
+				margin: 0 auto;
+			}
+		</style>
     </div>
     <!-- //CENTER -->
 
