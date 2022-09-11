6e5b115c2605b82ffda5f9f0fba9c2586bde0806
jhonata <jhonatatnt@hotmail.com>
2022-09-11 02:25:31 -0300

    Update mn_pri.php

--- includes/mn_pri.php
+++ includes/mn_pri.php
@@ -1,24 +1,36 @@
 <!-- //MENU PRINCIPAL -->
-<div id="mn_pri">
-    <div id="search_bar">
-        <input type="text" value="O que você Procura?" name="pesquisa">
-        <button><img id="icn_search" src="../img/icn_search.png"></button>
-    </div>
-    <div id="it_options">
-        <ul>
-            <li><a href="home.php">Home</a></li>
-            <li><a href="cadastro.php">Cadastro</a></li>
-            <li><a href="comunicacao.php">Comunicação</a></li>
-            <li><a href="monitoria.php">Monitoria</a></li>
-        </ul>
-        <div id="bar_selection"></div>
-    </div>
-    <div id="it_perfil">
-        <h2>Bem vindo, Pikachu</h2>
-        <a href="perfil.php">
-            <img src="https://sm.ign.com/ign_br/screenshot/default/025pikachu-os-anime-4_3pym.png">
-        </a>
-        <button><img src="../img/icn_msg.png"></button>
+<div id="container_mn_pri">
+    <div id="mn_pri">
+
+        <button id="icn_mn_sidebar" onclick="mn_sidebar()"><img src="../img/icn_mn_sidebar.png"></button>
+        <h2 id="title_section"><?php echo('Home')?></h2>
+        
+        <div id="search_bar">
+            <input type="text" value="O que você Procura?" name="pesquisa">
+            <button><img id="icn_search" src="../img/icn_search.png"></button>
+        </div>
+        
+        <div id="it_options">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="cadastro.php">Cadastro</a></li>
+                <li><a href="comunicacao.php">Comunicação</a></li>
+                <li><a href="monitoria.php">Monitoria</a></li>
+            </ul>
+            <div id="bar_selection"></div>
+        </div>
+        
+        <div id="it_perfil">
+            <h2>Bem vindo, Pikachu</h2>
+
+            <button><img src="../img/icn_msg.png"></button>
+            
+            <a href="perfil.php">
+                <img src="https://sm.ign.com/ign_br/screenshot/default/025pikachu-os-anime-4_3pym.png">
+            </a>
+            
+        </div>
+   
     </div>
 </div>
 <!-- //MENU PRINCIPAL -->
