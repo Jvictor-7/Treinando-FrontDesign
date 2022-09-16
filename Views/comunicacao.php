afd363088894bc6c2a4fe18d3b28b1a792a7f91c
jhonata <jhonatatnt@hotmail.com>
2022-09-16 14:06:09 -0300

    Update comunicacao.php

--- Views/comunicacao.php
+++ Views/comunicacao.php
@@ -6,7 +6,7 @@
 ?>
 
 	<!-- CENTER -->
-	<div class="container">
+    <div class="container">
 		<div class="name">
 			<style type="text/css">
 				.name{
@@ -22,7 +22,7 @@
 				}
 			</style>
 
-			<a href="../">Plataforma</a><a href=""> / Comunicação</a>
+			<a href="../">Plataforma</a><a href=""> / Cadastro</a>
 
 		</div>
 		
@@ -40,9 +40,12 @@
 				}
 				#title_pag{
 					font-family: 'segoe ui';
+					font-weight: 500;
 					font-size: 32px;
 				}
 				#subtitle_pag{
+					margin-top: -10px;
+					margin-bottom: 35px;
 					font-family: 'segoe ui';
 					font-size: 18px;
 					font-weight: normal;
@@ -52,14 +55,68 @@
 
 		<div id="div_title_pag">
 			<h1 id="title_pag">Painel de informações</h1>
-			<h2 id="subtitle_pag">Avisos institucionais e mensagens</h2>
+			<h2 id="subtitle_pag">Dados e estatísticas gerais</h2>
 		</div>
 
-		<div>
-			</br></br>
-			<a href="?x=57">lista de noticias</a>
-			</br></br>
-			<a href="">lista de notificacoes e mensagens</a>
+		<style type="text/css">
+
+			#list_BD h1, #container_statistic h1{
+				display: block;
+				width: 100%;
+				font-size: 22px;
+				font-family: 'Segoe UI';
+				font-weight: normal;
+			}
+			.borda{
+				display: block;
+				margin: 5px 0 25px 0;
+				width: 45px;
+				height: 4px;
+				background-color: #000;
+				border-radius: 2px;
+			}
+
+			#list_BD, #container_statistic{
+				display: block;
+				float: left;
+				/*border: 1px solid #000;*/
+				margin-left: 40px;
+			}
+			#list_BD{
+				width: calc(30% - 40px);
+				}
+				#list_BD li{list-style: none;}
+				#list_BD li a{
+					display: inline-block;
+					text-decoration: none;
+					margin-bottom: 10px;
+					padding: 7.5px 25px;
+					border-radius: 30px;
+					background-color: rgba(0,0,0,0.05);
+					color: #142748;
+				}
+			#list_BD li a:hover{background-color: rgba(0,0,0,0.10);}
+
+			#container_statistic{
+				width: calc(70% - 40px);
+			}
+			#container_statistic img{
+				width: 100%;
+			}
+		</style>
+
+		<ul id="list_BD">
+			<h1>Banco de dados</h1>
+			<div class="borda"></div>
+
+			<li><a href="cadastro/list_users.php?x=57">Lista de notícias</a></li>
+			<li><a href="cadastro/list_campi.php">Lista de notificações</a></li>		
+		</ul>
+
+		<div id="container_statistic">
+			<h1>Estatíticas</h1>
+			<div class="borda"></div>
+			<img src="../img/statistic.png">
 		</div>
     </div>
     <!-- //CENTER -->
